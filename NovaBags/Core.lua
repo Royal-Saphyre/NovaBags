aca2f4d9450597b53cfdb097fca26ddc577556d6
Royal-Saphyre <adisorvaschi6@gmail.com>
2026-07-22 16:19:35 +0200

    Create NovaBags main frame and functionality

---
+++ NovaBags/Core.lua
@@ -0,0 +1,71 @@
+--=============================================================================
+-- NovaBags
+-- File: Core.lua
+--
+-- Creates the main Nova window
+--=============================================================================
+
+NovaFrame = CreateFrame("Frame", "NovaMainFrame", UIParent)
+
+NovaFrame:SetSize(600, 450)
+NovaFrame:SetPoint("CENTER")
+
+NovaFrame:SetBackdrop({
+    bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
+    edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+    edgeSize = 16,
+    insets = {
+        left = 5,
+        right = 5,
+        top = 5,
+        bottom = 5
+    }
+})
+
+NovaFrame:SetMovable(true)
+NovaFrame:EnableMouse(true)
+
+NovaFrame:RegisterForDrag("LeftButton")
+
+NovaFrame:SetScript("OnDragStart", function(self)
+    self:StartMoving()
+end)
+
+NovaFrame:SetScript("OnDragStop", function(self)
+    self:StopMovingOrSizing()
+end)
+
+
+NovaFrame:Hide()
+
+
+-- Close button
+
+local close = CreateFrame(
+    "Button",
+    nil,
+    NovaFrame,
+    "UIPanelCloseButton"
+)
+
+close:SetPoint("TOPRIGHT", -5, -5)
+
+close:SetScript("OnClick", function()
+    NovaFrame:Hide()
+end)
+
+
+
+-- Slash command
+
+SLASH_NOVA1 = "/nova"
+
+SlashCmdList["NOVA"] = function()
+
+    if NovaFrame:IsShown() then
+        NovaFrame:Hide()
+    else
+        NovaFrame:Show()
+    end
+
+end
