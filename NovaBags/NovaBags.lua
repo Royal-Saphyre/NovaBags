b44eb42e2667b40351ec1dffd11d57d1171b5cf2
Royal-Saphyre <adisorvaschi6@gmail.com>
2026-07-22 16:14:32 +0200

    create addon loader
    Added initial setup for NovaBags addon with PLAYER_LOGIN event.

--- NovaBags/NovaBags.lua
+++ NovaBags/NovaBags.lua
@@ -1,1 +1,21 @@
+--=============================================================================
+-- NovaBags
+-- File: NovaBags.lua
+--
+-- Main addon file
+--=============================================================================
 
+local addonName = "NovaBags"
+
+local frame = CreateFrame("Frame")
+
+frame:RegisterEvent("PLAYER_LOGIN")
+
+frame:SetScript("OnEvent", function(self, event)
+
+    if event == "PLAYER_LOGIN" then
+        print("|cffd4af37NovaBags|r loaded!")
+
+    end
+
+end)
