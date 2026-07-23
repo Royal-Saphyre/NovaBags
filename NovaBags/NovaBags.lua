4dd4ac17e8c32bd24b8379ba28f65f3e317f4a1c
Royal-Saphyre <adisorvaschi6@gmail.com>
2026-07-23 22:27:51 +0200

    Update success message on player login

--- NovaBags/NovaBags.lua
+++ NovaBags/NovaBags.lua
@@ -1,21 +1,16 @@
 --=============================================================================
 -- NovaBags
 -- File: NovaBags.lua
---
 -- Main addon file
 --=============================================================================
 
 local addonName = "NovaBags"
-
 local frame = CreateFrame("Frame")
 
 frame:RegisterEvent("PLAYER_LOGIN")
 
 frame:SetScript("OnEvent", function(self, event)
-
     if event == "PLAYER_LOGIN" then
-        print("|cffd4af37NovaBags|r loaded!")
-
+        print("|cffd4af37NovaBags|r loaded successfully!")
     end
-
 end)
