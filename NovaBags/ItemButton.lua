29660907df382c0ade0d1b95610ecc7b4dd24263
Royal-Saphyre <adisorvaschi6@gmail.com>
2026-07-23 20:15:03 +0200

    Implement Nova item button creation functionality
    This file contains the implementation of the Nova item button system, including button creation, icon setup, count text, highlight effects, and tooltip functionality.

---
+++ NovaBags/ItemButton.lua
@@ -0,0 +1,95 @@
+--=============================================================================
+-- NovaBags
+-- File: ItemButton.lua
+--
+-- Creates Nova item buttons
+--=============================================================================
+
+print("|cff00ff00ItemButton.lua LOADED|r")
+
+-- Storage for all item buttons
+NovaItemButtons = {}
+
+-- Create one item button
+function NovaCreateItemButton(parent, index)
+
+    local button = CreateFrame(
+        "Button",
+        "NovaItemButton"..index,
+        parent
+    )
+
+    -- Size
+    button:SetSize(40, 40)
+
+    -- Icon
+    button.icon = button:CreateTexture(
+        nil,
+        "BACKGROUND"
+    )
+
+    button.icon:SetAllPoints()
+
+    -- Count text
+    button.count = button:CreateFontString(
+        nil,
+        "OVERLAY"
+    )
+
+    button.count:SetFont(
+        "Fonts\\\\FRIZQT__.TTF",
+        12,
+        "OUTLINE"
+    )
+
+    button.count:SetPoint(
+        "BOTTOMRIGHT",
+        -2,
+        2
+    )
+
+    -- Highlight effect
+    button:SetHighlightTexture(
+        "Interface\\\\Buttons\\\\ButtonHilight-Square"
+    )
+
+    -- Tooltip
+    button:SetScript(
+        "OnEnter",
+        function(self)
+
+            if self.link then
+
+                GameTooltip:SetOwner(
+                    self,
+                    "ANCHOR_RIGHT"
+                )
+
+                GameTooltip:SetHyperlink(
+                    self.link
+                )
+
+                GameTooltip:Show()
+
+            end
+
+        end
+    )
+
+    button:SetScript(
+        "OnLeave",
+        function()
+
+            GameTooltip:Hide()
+
+        end
+    )
+
+    -- Save reference
+    NovaItemButtons[index] = button
+
+    return button
+
+end
+
+print("|cff00ff00Nova ItemButton system ready!|r")
