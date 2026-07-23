9008f124f57aaaa45817175bb941af70ecca0228
Royal-Saphyre <adisorvaschi6@gmail.com>
2026-07-23 21:38:57 +0200

    Refactor NovaCreateItemButton for improved clarity

--- NovaBags/ItemButton.lua
+++ NovaBags/ItemButton.lua
@@ -1,66 +1,29 @@
 --=============================================================================
 -- NovaBags
 -- File: ItemButton.lua
+--
+-- Creates bag slots
 --=============================================================================
 
 NovaItemButtons = {}
 
 
-function NovaCreateItemButton(parent, index)
+
+function NovaCreateItemButton(parent,index)
 
 
-    local button = CreateFrame(
+    local button =
+    CreateFrame(
         "Button",
         "NovaItemButton"..index,
         parent
     )
 
 
+
     button:SetSize(
-        30,
-        30
-    )
-
-
-    button:SetFrameLevel(
-        parent:GetFrameLevel() + 5
-    )
-
-
-
-    button.icon = button:CreateTexture(
-        nil,
-        "ARTWORK"
-    )
-
-
-    button.icon:SetAllPoints()
-
-
-
-    button.count = button:CreateFontString(
-        nil,
-        "OVERLAY"
-    )
-
-
-    button.count:SetFont(
-        "Fonts\\FRIZQT__.TTF",
-        10,
-        "OUTLINE"
-    )
-
-
-    button.count:SetPoint(
-        "BOTTOMRIGHT",
-        -1,
-        1
-    )
-
-
-
-    button:SetHighlightTexture(
-        "Interface\\Buttons\\ButtonHilight-Square"
+        32,
+        32
     )
 
 
@@ -72,75 +35,172 @@ function NovaCreateItemButton(parent, index)
 
 
 
-    button:SetScript(
-        "OnClick",
-        function(self, mouse)
-
-
-            if not self.bagID then
-                return
-            end
-
-
-            if mouse == "LeftButton" then
-
-                PickupContainerItem(
-                    self.bagID,
-                    self.slotID
-                )
-
-
-            elseif mouse == "RightButton" then
-
-                UseContainerItem(
-                    self.bagID,
-                    self.slotID
-                )
-
-            end
-
-
-        end
+    button:RegisterForDrag(
+        "LeftButton"
     )
 
 
 
-    button:SetScript(
-        "OnEnter",
-        function(self)
+    ---------------------------------------------------
+    -- Icon
+    ---------------------------------------------------
 
-            if self.link then
+    button.icon =
+    button:CreateTexture(
+        nil,
+        "ARTWORK"
+    )
 
-                GameTooltip:SetOwner(
-                    self,
-                    "ANCHOR_RIGHT"
-                )
 
-                GameTooltip:SetHyperlink(
-                    self.link
-                )
+    button.icon:SetAllPoints()
 
-                GameTooltip:Show()
 
-            end
 
-        end
+    ---------------------------------------------------
+    -- Count
+    ---------------------------------------------------
+
+    button.count =
+    button:CreateFontString(
+        nil,
+        "OVERLAY"
+    )
+
+
+    button.count:SetFont(
+        "Fonts\\FRIZQT__.TTF",
+        11,
+        "OUTLINE"
+    )
+
+
+    button.count:SetPoint(
+        "BOTTOMRIGHT",
+        -2,
+        1
     )
 
 
 
-    button:SetScript(
-        "OnLeave",
-        function()
+    ---------------------------------------------------
+    -- Slot border
+    ---------------------------------------------------
 
-            GameTooltip:Hide()
+    button:SetNormalTexture(
+        "Interface\\Buttons\\UI-Quickslot2"
+    )
 
-        end
+
+    button:SetHighlightTexture(
+        "Interface\\Buttons\\ButtonHilight-Square"
     )
 
 
 
-    NovaItemButtons[index] = button
+    ---------------------------------------------------
+    -- Click
+    ---------------------------------------------------
+
+    button:SetScript(
+    "OnClick",
+    function(self,mouse)
+
+
+        if not self.bagID then
+            return
+        end
+
+
+
+        if mouse == "LeftButton" then
+
+
+            PickupContainerItem(
+                self.bagID,
+                self.slotID
+            )
+
+
+        elseif mouse == "RightButton" then
+
+
+            UseContainerItem(
+                self.bagID,
+                self.slotID
+            )
+
+
+        end
+
+
+    end)
+
+
+
+    ---------------------------------------------------
+    -- Drag
+    ---------------------------------------------------
+
+    button:SetScript(
+    "OnDragStart",
+    function(self)
+
+
+        PickupContainerItem(
+            self.bagID,
+            self.slotID
+        )
+
+
+    end)
+
+
+
+    ---------------------------------------------------
+    -- Tooltip
+    ---------------------------------------------------
+
+    button:SetScript(
+    "OnEnter",
+    function(self)
+
+
+        if self.link then
+
+
+            GameTooltip:SetOwner(
+                self,
+                "ANCHOR_RIGHT"
+            )
+
+
+            GameTooltip:SetHyperlink(
+                self.link
+            )
+
+
+            GameTooltip:Show()
+
+
+        end
+
+
+    end)
+
+
+
+    button:SetScript(
+    "OnLeave",
+    function()
+
+        GameTooltip:Hide()
+
+    end)
+
+
+
+    NovaItemButtons[index]=button
+
 
 
     return button
