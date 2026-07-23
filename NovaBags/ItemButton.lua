e9fafb08dd3b68587a5947009b459cf999e1b3dc
Royal-Saphyre <adisorvaschi6@gmail.com>
2026-07-23 22:19:23 +0200

    Refactor NovaCreateItemButton function

--- NovaBags/ItemButton.lua
+++ NovaBags/ItemButton.lua
@@ -5,140 +5,56 @@
 
 NovaItemButtons = {}
 
+function NovaCreateItemButton(parent, index)
+    local name = "NovaItemButton" .. index
+    -- Inherit the standard WotLK item button template
+    local button = CreateFrame("Button", name, parent, "ContainerFrameItemButtonTemplate")
 
+    button:SetSize(32, 32)
+    button:SetFrameLevel(parent:GetFrameLevel() + 2)
 
-function NovaCreateItemButton(parent,index)
+    -- Reference icon and count directly from the WoW template
+    button.icon = _G[name .. "IconTexture"]
+    button.count = _G[name .. "Count"]
 
+    -- Hook Tooltip handling for 3.3.5a
+    button:SetScript("OnEnter", function(self)
+        if self.bagID and self.slotID then
+            GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+            GameTooltip:SetBagItem(self.bagID, self.slotID)
+            GameTooltip:Show()
+        end
+    end)
 
-local button =
-CreateFrame(
-"Button",
-"NovaItemButton"..index,
-parent
-)
+    button:SetScript("OnLeave", function()
+        GameTooltip:Hide()
+    end)
 
+    -- Item interaction (Right-Click to use/equip, Left-Click to pick up)
+    button:RegisterForClicks("LeftButtonUp", "RightButtonUp")
+    button:SetScript("OnClick", function(self, buttonPressed)
+        if not self.bagID or not self.slotID then return end
 
-button:SetSize(
-32,
-32
-)
+        if buttonPressed == "RightButton" then
+            UseContainerItem(self.bagID, self.slotID)
+        elseif buttonPressed == "LeftButton" then
+            PickupContainerItem(self.bagID, self.slotID)
+        end
+    end)
 
+    button:RegisterForDrag("LeftButton")
+    button:SetScript("OnDragStart", function(self)
+        if self.bagID and self.slotID then
+            PickupContainerItem(self.bagID, self.slotID)
+        end
+    end)
 
+    button:SetScript("OnReceiveDrag", function(self)
+        if self.bagID and self.slotID then
+            PickupContainerItem(self.bagID, self.slotID)
+        end
+    end)
 
-button.icon =
-button:CreateTexture(
-nil,
-"BACKGROUND"
-)
-
-
-button.icon:SetAllPoints()
-
-
-
-button.count =
-button:CreateFontString(
-nil,
-"OVERLAY"
-)
-
-
-button.count:SetFont(
-"Fonts\\FRIZQT__.TTF",
-10,
-"OUTLINE"
-)
-
-
-button.count:SetPoint(
-"BOTTOMRIGHT",
--2,
-2
-)
-
-
-
-button:SetHighlightTexture(
-"Interface\\Buttons\\ButtonHilight-Square"
-)
-
-
-
-button:SetScript(
-"OnEnter",
-function(self)
-
-if self.link then
-
-GameTooltip:SetOwner(
-self,
-"ANCHOR_RIGHT"
-)
-
-GameTooltip:SetHyperlink(
-self.link
-)
-
-GameTooltip:Show()
-
-end
-
-end
-)
-
-
-
-button:SetScript(
-"OnLeave",
-function()
-GameTooltip:Hide()
-end
-)
-
-
-
-button:RegisterForClicks(
-"LeftButtonUp",
-"RightButtonUp"
-)
-
-
-
-button:SetScript(
-"OnClick",
-function(self,button)
-
-
-if not self.bagID then return end
-
-
-if button=="RightButton" then
-
-UseContainerItem(
-self.bagID,
-self.slotID
-)
-
-
-elseif button=="LeftButton" then
-
-PickupContainerItem(
-self.bagID,
-self.slotID
-)
-
-
-end
-
-
-end
-)
-
-
-
-NovaItemButtons[index]=button
-
-
-return button
-
+    NovaItemButtons[index] = button
+    return button
 end
