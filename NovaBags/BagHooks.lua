370c0091c01ac5dd58e587cf6303dccbba530af7
Royal-Saphyre <adisorvaschi6@gmail.com>
2026-07-23 22:28:51 +0200

    Refactor bag toggle functions to use hooks

--- NovaBags/BagHooks.lua
+++ NovaBags/BagHooks.lua
@@ -1,8 +1,7 @@
 --=============================================================================
 -- NovaBags
 -- File: BagHooks.lua
---
--- Safely replaces Blizzard bag opening functions and keybindings
+-- Safely hooks Blizzard bag toggles
 --=============================================================================
 
 local function ToggleNova()
@@ -14,47 +13,19 @@ local function ToggleNova()
     end
 end
 
-------------------------------------------------
--- Global toggle function for bindings
-------------------------------------------------
-_G["NovaToggleBags"] = ToggleNova
-
-------------------------------------------------
--- Override Blizzard bag toggles
-------------------------------------------------
-function ToggleBackpack() ToggleNova() end
-function OpenBackpack() NovaFrame:Show() NovaDisplayItems() end
-function CloseBackpack() NovaFrame:Hide() end
-function ToggleAllBags() ToggleNova() end
-function OpenAllBags() NovaFrame:Show() NovaDisplayItems() end
-function CloseAllBags() NovaFrame:Hide() end
-
--- Intercept microbar bag icon clicks (Bag 0 to 4)
-hooksecurefunc("ToggleBag", function(bagID)
+hooksecurefunc("ToggleBackpack", function()
     ToggleNova()
 end)
 
-hooksecurefunc("OpenBag", function(bagID)
+hooksecurefunc("ToggleBag", function()
+    ToggleNova()
+end)
+
+hooksecurefunc("OpenAllBags", function()
     NovaFrame:Show()
     NovaDisplayItems()
 end)
 
-------------------------------------------------
--- Force override standard B and Shift+B keys
-------------------------------------------------
-local f = CreateFrame("Frame")
-f:RegisterEvent("PLAYER_LOGIN")
-f:SetScript("OnEvent", function(self, event)
-    -- Hide default Blizzard container frames if they pop up
-    for i = 1, NUM_CONTAINER_FRAMES do
-        local container = _G["ContainerFrame" .. i]
-        if container then
-            container:UnregisterAllEvents()
-            container:Hide()
-        end
-    end
-
-    -- Override standard WoW keybindings to point to NovaBags
-    SetBinding("B", "NOVA")
-    SetBinding("SHIFT-B", "NOVA")
+hooksecurefunc("CloseAllBags", function()
+    NovaFrame:Hide()
 end)
