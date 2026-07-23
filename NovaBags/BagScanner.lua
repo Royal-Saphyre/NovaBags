39938af7a734ac4aea6cd7772a450c4ef5fc94f7
Royal-Saphyre <adisorvaschi6@gmail.com>
2026-07-23 22:41:48 +0200

    Update BagScanner.lua

--- NovaBags/BagScanner.lua
+++ NovaBags/BagScanner.lua
@@ -15,13 +15,15 @@ function NovaScanBags()
             for slot = 1, slots do
                 local texture, count, locked, quality, readable, lootable, link = GetContainerItemInfo(bag, slot)
 
-                table.insert(NovaInventory, {
-                    bagID = bag,
-                    slotID = slot,
-                    link = link,
-                    count = (count and count > 1) and count or "",
-                    texture = texture
-                })
+                if texture then
+                    table.insert(NovaInventory, {
+                        bagID = bag,
+                        slotID = slot,
+                        link = link,
+                        count = (count and count > 1) and count or "",
+                        texture = texture
+                    })
+                end
             end
         end
     end
@@ -31,6 +33,6 @@ local scanner = CreateFrame("Frame")
 scanner:RegisterEvent("BAG_UPDATE")
 scanner:SetScript("OnEvent", function()
     if NovaFrame and NovaFrame:IsShown() then
-        NovaDisplayItems()
+        NovaDisplayItems(false)
     end
 end)
